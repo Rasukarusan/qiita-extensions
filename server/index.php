6f7ffdac7c7c76217c4a293bb1afc72ecd09c26d
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-12-09 00:24:39 +0900

    不要なファイルを削除

--- server/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// curlを叩くマナーとして遅延入れておきましょうね。
-sleep(1);
-$url = $_GET['url'];
-$like_cnt = shell_exec('curl -s '.$url.' | tr "<" "\n" |grep "it-Actions_likeCount" | sed "s/a.*>//g"');
-echo $like_cnt;
