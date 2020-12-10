d4ee5ec621c3e907d2084f426cd4d70d0dc712aa
Rasukarusan <gta_youtube@yahoo.co.jp>
2020-12-10 18:08:00 +0900

    ボタン押下時に色が変わるようにした

--- ChromeExtension/js/main.js
+++ ChromeExtension/js/main.js
@@ -1,29 +1,29 @@
-function getLikes() {
-    $('.adventCalendarCalendar_comment').each(function(){
-        var url = $(this).children('a').attr('href')
-        if(typeof url === 'undefined') return
-         $.ajax({
-             url:url,
-             type:'GET',
-             dataType: 'html',
-        })
-        .done( (data) => {
-            let likes = data.match(/likesCount":([0-9]*),/)
-            let likeCount = likes[1]
-            $(this).after('<i class="fa fa-fw fa-thumbs-up"></i>'+likeCount)
-
-            let pv = data.match(/totalPv":([0-9]*),/)
-            let pvCount = pv[1]
-            $(this).after('<i class="fa fa-fw fa-eye"></i>'+pvCount+'<br/>')
-        })
-        .fail( (data) => {
-        })
-        .always( (data) => {
-        });
+const getLikes = () => {
+  $('.adventCalendarCalendar_comment').each(function(){
+    const url = $(this).children('a').attr('href')
+    if(typeof url === 'undefined') return
+     $.ajax({
+       url,
+       type:'GET',
+       dataType: 'html',
     })
+    .done(data => {
+      const likes = data.match(/likesCount":([0-9]*),/)
+      const likeCount = likes[1]
+      $(this).after('<i class="fa fa-fw fa-thumbs-up"></i>'+likeCount)
+
+      const pv = data.match(/totalPv":([0-9]*),/)
+      const pvCount = pv[1]
+      $(this).after('<i class="fa fa-fw fa-eye"></i>'+pvCount+'<br/>')
+    })
+  })
 }
-let button = '<div id="showButtonContainer"><button id="showLikeButton" style="border"><i class="fa fa-bell"></i> 表示</button></div>'
+const button = '<div id="showButtonContainer"><button id="showLikeButton" style="border"><i class="fa fa-bell"></i> 表示</button></div>'
 $('.container' + '.adventCalendarSection').after(button)
 $('#showLikeButton').click(function() {
-    getLikes()
+  getLikes()
+  $('#showLikeButton').css({
+    'background': '#D04255',
+    'border-bottom': 'solid 4px #E38692',
+  })
 })
