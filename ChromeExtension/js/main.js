496db1f28bbe6f394628c965602f03b5c196b7cf
Rasukarusan <gta_youtube@yahoo.co.jp>
2020-12-10 18:36:59 +0900

    refactor

--- ChromeExtension/js/main.js
+++ ChromeExtension/js/main.js
@@ -1,3 +1,5 @@
+const BUTTON_ID = 'showLikeButton'
+
 const getLikes = () => {
   $('.adventCalendarCalendar_comment').each(function(){
     const url = $(this).children('a').attr('href')
@@ -8,22 +10,38 @@ const getLikes = () => {
        dataType: 'html',
     })
     .done(data => {
-      const likes = data.match(/likesCount":([0-9]*),/)
+      const likes = data.match(/"likesCount":([0-9]*),/)
       const likeCount = likes[1]
       $(this).after('<i class="fa fa-fw fa-thumbs-up"></i>'+likeCount)
 
-      const pv = data.match(/totalPv":([0-9]*),/)
+      const pv = data.match(/"totalPv":([0-9]*),/)
       const pvCount = pv[1]
       $(this).after('<i class="fa fa-fw fa-eye"></i>'+pvCount+'<br/>')
     })
   })
 }
-const button = '<div id="showButtonContainer"><button id="showLikeButton" style="border"><i class="fa fa-bell"></i> 表示</button></div>'
-$('.container' + '.adventCalendarSection').after(button)
-$('#showLikeButton').click(function() {
-  getLikes()
-  $('#showLikeButton').css({
+
+const addShowButton = () => {
+  const button = `
+    <div id="showButtonContainer">
+      <button id="${BUTTON_ID}" style="border">
+        <i class="fa fa-bell"></i> 表示
+      </button>
+    </div>
+  `
+  $('.container' + '.adventCalendarSection').after(button)
+}
+
+const changeButtonColor = () => {
+  $(`#${BUTTON_ID}`).css({
     'background': '#D04255',
     'border-bottom': 'solid 4px #E38692',
   })
+}
+
+addShowButton()
+
+$(`#${BUTTON_ID}`).click(function() {
+  getLikes()
+  changeButtonColor()
 })
