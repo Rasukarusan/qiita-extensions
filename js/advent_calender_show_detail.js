0eddcdf7c4f8f67bead3bd0796a63d511e749d30
Rasukarusan <gta_youtube@yahoo.co.jp>
2021-01-01 17:01:09 +0900

    URL判定を追加

--- js/advent_calender_show_detail.js
+++ js/advent_calender_show_detail.js
@@ -2,29 +2,31 @@
  * アドベントカレンダーにいいね数、ストック数、View数を表示する
  */
 (function() {
-$('.adventCalendarCalendar_leave').css({'z-index': -1})
+  if (!location.pathname.match(/advent-calendar/)) return
 
-const getLikes = () => {
-  $('.adventCalendarCalendar_comment').each(function(){
-    const url = $(this).children('a').attr('href')
-    if(typeof url === 'undefined') return
-    const hostname = new URL(url).hostname
-    if (hostname !== 'qiita.com') return
-     $.ajax({ url, dataType: 'html' }).done(data => {
-      const stock = data.match(/"stockedCount":([0-9]*),/)
-      const stockCount = stock[1]
-      $(this).after('<p><i class="fa fa-fw fa-archive"></i>'+stockCount+'</p>')
+  $('.adventCalendarCalendar_leave').css({'z-index': -1})
 
-      const likes = data.match(/"likesCount":([0-9]*),/)
-      if(!likes) return
-      const likeCount = likes[1]
-      $(this).after('<p><i class="fa fa-fw fa-thumbs-up"></i>'+likeCount+'</p>')
+  const getLikes = () => {
+    $('.adventCalendarCalendar_comment').each(function(){
+      const url = $(this).children('a').attr('href')
+      if(typeof url === 'undefined') return
+      const hostname = new URL(url).hostname
+      if (hostname !== 'qiita.com') return
+       $.ajax({ url, dataType: 'html' }).done(data => {
+        const stock = data.match(/"stockedCount":([0-9]*),/)
+        const stockCount = stock[1]
+        $(this).after('<p><i class="fa fa-fw fa-archive"></i>'+stockCount+'</p>')
 
-      const pv = data.match(/"totalPv":([0-9]*),/)
-      const pvCount = pv[1]
-      $(this).after('<p><i class="fa fa-fw fa-eye"></i>'+pvCount+'</p>')
+        const likes = data.match(/"likesCount":([0-9]*),/)
+        if(!likes) return
+        const likeCount = likes[1]
+        $(this).after('<p><i class="fa fa-fw fa-thumbs-up"></i>'+likeCount+'</p>')
+
+        const pv = data.match(/"totalPv":([0-9]*),/)
+        const pvCount = pv[1]
+        $(this).after('<p><i class="fa fa-fw fa-eye"></i>'+pvCount+'</p>')
+      })
     })
-  })
-}
-getLikes()
+  }
+  getLikes()
 })()
--- js/show_detail.js
+++ js/show_detail.js
@@ -2,8 +2,9 @@
  * 他人の記事でもView数を表示する
  */
 (function() {
+  if (!location.pathname.match(/items/)) return
+
   const pv = document.body.innerHTML.match(/"totalPv":([0-9]*),/)[1]
-  console.log($('.it-Header_pv') );
   if ($('.it-Header_pv').length === 0) {
     $('.it-Header_meta').prepend(`<div class="it-Header_pv">${pv} views</div>`)
   }
