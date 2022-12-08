c7e96d25582691f086ee655ead6ca27d94bef6c9
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-12-08 11:54:36 +0900

    アドベントカレンダー2022に対応

--- js/advent_calender_show_detail.js
+++ js/advent_calender_show_detail.js
@@ -1,5 +1,6 @@
 /**
  * アドベントカレンダーにいいね数、ストック数、View数を表示する
+ * 2020年版
  */
 (function() {
   if (!location.pathname.match(/advent-calendar/)) return
@@ -30,3 +31,48 @@
   }
   getLikes()
 })()
+
+/**
+ * アドベントカレンダーにいいね数、ストック数、View数を表示する
+ * 2021年以降
+ */
+window.onload = function() {
+  if (!location.pathname.match(/advent-calendar/)) return
+
+  /**
+   * アイコンカウントの要素を生成
+   */
+  function createCountElement(className, count) {
+    const container = document.createElement("p")
+    const icon = document.createElement("i");
+    icon.setAttribute("class", `fa fa-fw ${className}`)
+    const countElement = document.createTextNode(count)
+    container.appendChild(icon)
+    container.appendChild(countElement)
+    return container
+  }
+
+  // アドベントカレンダー2021からクラス名が予測不可能なものになったため、aタグを全部抽出して振り分ける
+  const aTags = document.getElementsByTagName("a")
+  // アイコンカウント要素を追加
+  Array.prototype.forEach.call(aTags, (aTag) => {
+    if (aTag.href.match(/https:\/\/qiita.com\/.*\/items\//)) {
+      const url = aTag.href
+      const parent = aTag.parentElement
+      $.ajax({ url, dataType: 'html' }).done(data => {
+        const pv = data.match(/"totalPv":([0-9]*),/)
+        const pvCount = pv[1]
+        parent.insertBefore(createCountElement('fa-eye', pvCount), aTag.nextSliding)
+
+        const stock = data.match(/"stockedCount":([0-9]*),/)
+        const stockCount = stock[1]
+        parent.insertBefore(createCountElement('fa-archive', stockCount), aTag.nextSliding)
+
+        const likes = data.match(/"likesCount":([0-9]*),/)
+        const likeCount = likes[1]
+        parent.insertBefore(createCountElement('fa-thumbs-up', likeCount), aTag.nextSliding)
+
+      })
+    }
+  })
+}
